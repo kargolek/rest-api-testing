3a8fd7cafc040cf19c4b250c548d49dc1c8c6ed7
Karol Kuta-Orlowicz <k.kuta@samsung.local>
2020-01-24 13:22:42 +0100

    add pojo Board; add new test post new board

--- src/test/java/org/api/test/BoardRestTest.java
+++ src/test/java/org/api/test/BoardRestTest.java
@@ -3,18 +3,18 @@ package org.api.test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Method;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.api.test.pojo.Board;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItems;
+import static io.restassured.RestAssured.given;;
+import static org.hamcrest.Matchers.*;
 
 public class BoardRestTest {
 
     private RequestSpecification requestSpecification;
-    private Response response;
+    private Board board;
 
     @BeforeGroups(groups = "set_baseURI")
     public void init() {
@@ -43,7 +43,7 @@ public class BoardRestTest {
                 .queryParam("prefs_cardAging", "regular")
                 .queryParam("key", System.getenv("trl_key"))
                 .queryParam("token", System.getenv("trl_token"));
-        response = requestSpecification.request(Method.POST, "boards");
+        board = requestSpecification.request(Method.POST, "boards").as(Board.class);
     }
 
     /**
@@ -63,16 +63,49 @@ public class BoardRestTest {
                 .body("name", hasItems("Example_Name_Board"));
     }
 
+    @Test(groups = "set_baseURI")
+    public void post_board_name_test_api_board_created() {
+        given()
+                .contentType(ContentType.TEXT)
+                .queryParam("", "?")
+                .queryParam("name", "test_api_board_created")
+                .queryParam("defaultLabels", "true")
+                .queryParam("defaultLists", "true")
+                .queryParam("desc", "test description")
+                .queryParam("keepFromSource", "none")
+                .queryParam("prefs_permissionLevel", "private")
+                .queryParam("prefs_voting", "disabled")
+                .queryParam("prefs_comments", "members")
+                .queryParam("prefs_invitations", "members")
+                .queryParam("prefs_selfJoin", "true")
+                .queryParam("prefs_cardCovers", "true")
+                .queryParam("prefs_background", "green")
+                .queryParam("prefs_cardAging", "regular")
+                .queryParam("key", System.getenv("trl_key"))
+                .queryParam("token", System.getenv("trl_token"))
+                .when()
+                .post("boards")
+                .then()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .body("name", is("test_api_board_created"))
+                .body("desc", is("test description"));
+    }
+
     @Test(groups = "set_baseURI_create_board")
     public void put_board_by_id_new_name() {
-        String endpoint = "boards/" + response.getBody().jsonPath().get("id");
         given()
                 .queryParam("name", "new_changed_name")
                 .queryParam("key", System.getenv("trl_key"))
                 .queryParam("token", System.getenv("trl_token"))
                 .when()
-                .put(endpoint)
+                .put(String.format("boards/%s", board.getId()))
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .body("name", is("new_changed_name"));
     }
+
+
 }
+
---
+++ src/test/java/org/api/test/pojo/Board.java
@@ -0,0 +1,67 @@
+package org.api.test.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Board {
+
+    private String id;
+    private String name;
+    private String descData;
+    private boolean closed;
+    private String idOrganization;
+    private String idEnterprise;
+    private boolean pinned;
+    private String url;
+
+    public Board(@JsonProperty("id") String id, @JsonProperty("name") String name, @JsonProperty("descData") String descData,
+                 @JsonProperty("closed") boolean closed, @JsonProperty("idOrganization") String idOrganization, @JsonProperty("idEnterprise") String idEnterprise,
+                 @JsonProperty("pinned") boolean pinned, @JsonProperty("url") String url) {
+        this.id = id;
+        this.name = name;
+        this.descData = descData;
+        this.closed = closed;
+        this.idOrganization = idOrganization;
+        this.idEnterprise = idEnterprise;
+        this.pinned = pinned;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+   public String getDescData() {
+        return descData;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public String getIdOrganization() {
+        return idOrganization;
+    }
+
+    public String getIdEnterprise() {
+        return idEnterprise;
+    }
+
+    public boolean isPinned() {
+        return pinned;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String toString() {
+        return String.format("Board data: id: %s name: %s", getId(), getName());
+    }
+
+}
